f3cd9a98c23f2f99d11af6cddd52d498e50b77a6
gustavo <sandovalpalaciosgustavo@gmail.com>
2025-05-09 09:04:26 -0500

    test deploy 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Home } from "./pages/home/home";
+import { Home } from "./pages/home/Home";
 import { Plans } from "./pages/plans/Plans";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import { ProtegerRutas } from "./shared/proteger-rutas/ProtegerRutas";
